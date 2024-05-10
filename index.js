8de84807f854f5ec7c73cc90ab475a8b2525732e
Kirai-Kevin <167949441+Kirai-Kevin@users.noreply.github.com>
2024-05-10 08:30:49 +0300

    test commit

---
+++ index.js
@@ -0,0 +1,9 @@
+document.getElementById('myButton').addEventListener('click', function() {
+    window.location.href = '/game.html';
+});
+
+document.getElementById('signInBtn').addEventListener('click', function() {
+    window.location.href = '/signIn.html';
+});
+
+
